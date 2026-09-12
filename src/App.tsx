4c1a0595626c53bc626e1a3e03729ff690bbc8cf
rifat-17 <rifatahmedf4@gmail.com>
2026-09-12 15:54:53 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import HeroBanner from "./Components/HeroBanner"
+import Nav from "./Components/Nav"
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Nav />
+     <HeroBanner />
+    </>
+  )
+}
+
+export default App
